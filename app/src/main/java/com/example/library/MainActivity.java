6f85b1d686acd7b81e04de7e2930d3dbf503d037
MrFaktum <kirtamal@gmail.com>
2024-03-14 17:50:01 +0300

    nadpis poiska ne stiraetsa ona podskazka, dobavil svg v fytor i on ne lezet na verh + kartinka s silki v Java

--- app/src/main/java/com/example/library/MainActivity.java
+++ app/src/main/java/com/example/library/MainActivity.java
@@ -1,29 +1,43 @@
 package com.example.library;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.Bundle;
-
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.PopupMenu;
 import androidx.appcompat.app.AppCompatActivity;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.CustomTarget;
+import com.bumptech.glide.request.transition.Transition;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button = findViewById(R.id.button4);
-        button.setOnClickListener(new View.OnClickListener() {
+        Button button = findViewById(R.id.button);
+        Glide.with(this)
+                .load("https://litres.ru/pub/c/cover/6130095.jpg")
+                .into(new CustomTarget<Drawable>() {
+                    @Override
+                    public void onResourceReady(Drawable resource, Transition<? super Drawable> transition) {
+                        button.setBackground(resource);
+                    }
+
+                    @Override
+                    public void onLoadCleared(Drawable placeholder) {
+                        // Обработка очистки ресурса, если необходимо.
+                    }
+                });
+
+        Button button4 = findViewById(R.id.button4);
+        button4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 PopupMenu popup = new PopupMenu(MainActivity.this, v);
-                MenuInflater inflater = popup.getMenuInflater();
+                MenuInflater inflater = getMenuInflater();
                 inflater.inflate(R.menu.my_menu, popup.getMenu());
                 popup.show();
             }
